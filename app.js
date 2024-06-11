9bf15370e15ac3e735ab5053f4bfe569dabf5e32
Diagrasias <diagrasias.dev@gmail.com>
2024-06-11 12:02:25 +0300

    Ненужный код убран

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ const sequelize = require('./db');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Пример маршрута для получения данных о гостинице
-app.get('/', async (req, res) => {
-  const hotel = await models.Hotel.findByPk(req.params.id);
-  res.json(hotel);
-});
+
 
 // Запуск сервера
 sequelize.sync().then(() => {
